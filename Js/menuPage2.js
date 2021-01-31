5a48bee9e5139d93e4f0ad4ab12c538ab2c586f2
alane-tc <alantamez28@hotmail.com>
2021-01-30 18:51:58 -0600

    new files

---
+++ Js/menuPage2.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", function () {
+    let p10 = document.querySelector("#Hector-Salamanca")
+
+    let Hector = () => { window.location = 'Character/character10.html' }
+
+
+    p10.addEventListener("click", function () { Hector(); })
+
+})
